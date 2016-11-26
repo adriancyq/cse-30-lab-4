7c553b94f7a0363e88ce51b47a3c3317c73dd8ab
Jonathan Chiu <jonathanchiu@Jonathans-MacBook-Pro.local>
2016-11-26 15:51:58 -0800

    test string length

--- substring.s
+++ substring.s
@@ -8,16 +8,41 @@
 .type substring, %function
 
 substring:
+
     @ Save caller's registers on the stack
     push {r4-r11, ip, lr}
 
-    @ YOUR CODE GOES HERE (char *str1 is in r0, char *str2 is in r1)
+    @ Parameters:
+    @ R0: char * str1
+    @ R1: char * str2 
     @-----------------------
 
-    @ (your code)
+    @ Store arguments elsewhere
+    MOV R10, R0
+    MOV R11, R1 
+
+    @ Find the length of first string
+    MOV R0, R10
+    BL strlen
+    MOV R8, R0 
+
+    @ Find the length of the second string 
+    MOV R0, R11 
+    BL strlen 
+    MOV R9, R0 
+
+    @ Check if the first one is shorter than the second one
+    CMP R8, R9 
+
+    @ First one is shorter
+    MOVLT R0, #1
+
+    @ Second one is shorter
+    MOVGT R0, #0
+
 
     @ put your return value in r0 here:
-
+    @MOV R0, R8
     @-----------------------
 
     @ restore caller's registers
@@ -28,4 +53,3 @@ substring:
 .endfunc
 
 .end
-
