22743e1efd2bdf0a9eba2740eff0fa2f631d3166
adrian cordova y quiroz <adriancordovayquiroz@adrians-mbp-3.dynamic.ucsd.edu>
2016-11-28 13:28:41 -0800

    binary_searc_arm.s start

--- binary_search_ARM.s
+++ binary_search_ARM.s
@@ -19,7 +19,40 @@ binary_search_ARM:
     @ We need to save away a bunch of registers
     push    {r4-r11, ip, lr}
     @ May need to decrement stack pointer for more stack space
+    SUB SP, SP, #8
 
+    @compare start and end
+    CMP R1, R2
+
+    @branch to false declaration to make return value =1
+    BGT falseDec
+
+    @data[mid] == toFind
+    STR R1, [SP]
+    
+  	
+    STR R2, [sp, #4]
+
+    SUB R6, R2, R1
+    ASR R6, R6, #1
+    ADD R6, R6, R1
+
+    LDR R5, [R0, R6, LSL#2]
+
+    CMP R5, R3
+    MOVEQ R0, R6
+    BEQ return 
+
+    SUBGT R2, R6, #1
+
+    ADDLT R1, R6, #1
+
+    BL binary_search_ARM
+    B return
+
+    falseDec:
+    	MOV R0, #-1
+    return:
     /* You should probably do something here */
 
     mov r0, #0
