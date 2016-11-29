c950fcc51f74f1acb5c33970ca4a19095c8ceda4
adrian cordova y quiroz <adriancordovayquiroz@adrians-MBP-3.attlocal.net>
2016-11-28 21:59:43 -0800

    majority_ count_ARM attempt lol

--- majority_count_ARM.s
+++ majority_count_ARM.s
@@ -1,18 +1,9 @@
 .syntax unified
 
-/* You can put constants in the .data section. Look up how to do it on your own,
- * or come ask us if you're curious!*/
 .data 
 
 
 .text
-
-/* int majority_count_ARM(int * arr, int len, int * result)*/
-/* Remember to place your return value in r0. */
-/* Return the count of the majority element in arr, */
-/* or 0 if there is no majority. If "result" is not */
-/* NULL, place the value of the majority element at */
-/* that memory location before returning. */
 
 .align 8
 .global majority_count_ARM
@@ -24,10 +15,145 @@ majority_count_ARM:
     push    {r4-r11, ip, lr}
     @ May want to decrement stack pointer for more space
 
-    /* You should probably do something here */
+    @int *arr = R0
+    @int len = R1
+    @int *result = R2
 
-    mov r0, #0
+    @SUB SP, SP, #16 	allocated 16 bytes to stack
+    @STR R0, [SP] 		store R0 in stack pointer
+    @STR R1, [SP, #4]	store R1 in stack pointer
+    @STR R2, [SP, #8]	store R2 in stack pointer
+
+    @CMP R1, #0 		len == 0
+    @MOVEQ R0, #0 		return 0 because len == 0
+    @BEQ end 		branch to return
+
+    @CMP R1, #1 		compare len to 1
+    @BEQ rightMajority 	branch to rightMajority if equal to 1
+    @BNE count		branch to counterFunc if not equal to 1
+
+@rightMajority:
+	
+	@CMP R1, #0
+	@BEQ end
+	@LDR R2, [R0]
+	@CMP R1, #0
+	@STRNE R2, [R1]
+	@MOV R0, #1
+	@B end 
+
+@count:
+	
+	@LDR R0, [SP]
+	@LDR R3, [SP, #4]
+	@LSR R3, R3, #1
+	@ADD R1, SP, #12
+
+	@BL majority_count_ARM
+	@STR R0, [SP, #20]  		PROBABLY WRONG NUMBER OF bytes
+	@MOV R7, R0
+
+	@LDR R0, [SP]
+	@LDR R3, [SP, #4]
+	@LSR R4, R3, #1
+	@ADD R0, R0, R4
+	@SUB R3, R3, R4
+	@ADD R1, SP, #16
+
+	@BL majority_count_ARM
+	@STR R0, [SP, #24]
+	@MOV R6, R0
+
+	@CMP R7, #0
+	@BEQ majorityfinder 			
+	@LDR R0, [SP]
+	@LDR R3, [SP, #4]
+	@LDR R1, [SP, #12]
+
+	@B rightcount			
+
+@rightmajority_return:
+
+	@POP {R4-R7}
+	@MOV R5, R0
+	@CMP R0, R4
+	@BLE majorityfinder
+
+	@MOV R0, R5
+
+	@LDR R5, [SP, #8]
+
+	@CMP R5, #0
+	@BEQ end
+	@STR R1, [R5]
+	@B end
+
+@majorityfinder:
+	@CMP R6, #0
+	@BEQ end
+	@LDR R0, [SP]
+	@LDR R3, [SP, #4]
+	@LDR R1, [SP, #16]
+
+	@B leftcount
+
+@leftmajority_return:
+
+	@POP {R4-R7}
+	@MOV R5, R0
+	@CMP R0, R4
+	@MOVLE R0, #0
+	@BLE end
+
+	@MOV R0, R5
+	@LDR R5, [SP, #8]
+
+	@CMP R5, #0
+	@BEQ end
+	@STR R1, [R5]
+	@B end
+
+@rightcount:
+
+	@PUSH {R4-R7}
+	@MOV R5, #0
+	@MOV R6, #0
+
+@loop1:
+
+	@CMP R5, R3
+	@MOVEQ R0, R6
+	@BEQ rightmajority_return
+	@LDR R4, [R0, R5, LSL #2]
+	@CMP R4, R1
+	@ADDEQ R6, R6, #1
+	@ADD R5, R5, #1
+
+	@B loop1
+
+@leftcount:
+	
+	@PUSH {R4-R7}
+	@MOV R5, #0
+	@MOV R6, #0
+
+@loop2:
+	
+	@CMP R5, R3
+	@MOVEQ R0, R6
+	@BEQ leftmajority_return
+	@LDR R4, [R0, R5, LSL #2]
+	@CMP R4, R1
+	@ADDEQ R6, R6, #1
+	@ADD R5, R5, #1
+
+	@B loop2
+
+
     @ Remember to restore your stack pointer before popping!
+@end:
+	@ADD SP, SP, #32 
+    
     @ This handles restoring registers and returning
     pop     {r4-r11, ip, pc}
 
